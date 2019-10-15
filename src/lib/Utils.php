5c88ef92e201af31932404ad291494c9de5f88b2
karamush <zbcdefgh@gmail.com>
2019-10-15 15:35:49 +0300

    Добавились методы для определения isGet, isPost, а также для получения реального IP клиента (с учётом cloudflare и прокси).

--- src/lib/Utils.php
+++ src/lib/Utils.php
@@ -29,6 +29,47 @@ class Utils {
     }
 
     /**
+     * Это GET-запрос?
+     * @return bool
+     */
+    static function isGet() {
+        return self::RequestMethod()($_SERVER, 'REQUEST_METHOD') === 'GET';
+    }
+
+    /**
+     * Это POST-запрос?
+     * @return bool
+     */
+    static function isPost() {
+        return self::RequestMethod() === 'POST';
+    }
+
+    /**
+     * Получить REQUEST_METHOD запроса
+     * @return mixed|null
+     */
+    static function RequestMethod() {
+        return self::getArrayValue($_SERVER, 'REQUEST_METHOD');
+    }
+
+    /**
+     * Получить IP клиента с учётом Cloudflare и всяких прокси (непрозрачных)
+     * @return mixed|null
+     */
+    static function GetIP() {
+        // сначала cloudflare, если он есть
+        if ($cf_ip = self::getArrayValue($_SERVER, 'HTTP_CF_CONNECTING_IP')) {
+            return $cf_ip;
+        }
+
+        // теперь клиенты за прокси, а если нет, то просто REMOTE_ADDR
+        // WARNING! Щас будет мозголомно, но зато кратко... :)
+        return self::getArrayValue($_SERVER, 'HTTP_CLIENT_IP',
+                self::getArrayValue($_SERVER, 'HTTP_X_FORWARDED_FOR',
+                 self::getArrayValue($_SERVER, 'REMOTE_ADDR')));
+    }
+
+    /**
      * Pretty print array
      * @param $array
      */
